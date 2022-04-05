205d51a4ecbb0cbaa032b2aaa7f39f0dc1f94b47
Cameron <cpanzenbock@users.noreply.github.com>
2022-04-05 12:53:54 +1000

    Task 1: completed User class

--- ctrlr/user.py
+++ ctrlr/user.py
@@ -1,10 +1,17 @@
 class User:
     # this is the function used to create the user
     def __init__(self):
-        return None
+        self.user_type='guest'
+        self.uname=None
+        self.pwd=None
+        self.emailID=None
 
     def register(self,name,pwd,emailID):
-        return None
+        self.uname=name
+        self.pwd=pwd
+        self.emailID=emailID
 
     def __repr__(self):
-        return None
+        s= "Name: {0}, Email: {1}, type: {2}\n"
+        s=s.format(self.uname, self.emailID, self.user_type)
+        return s
