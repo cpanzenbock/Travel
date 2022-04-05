28c8bd77dbca6baee7a26847e5a3b06bd4e465e1
Cameron <cpanzenbock@users.noreply.github.com>
2022-04-05 13:10:06 +1000

    Task 3: completed Booking class

--- ctrlr/booking.py
+++ ctrlr/booking.py
@@ -3,7 +3,15 @@ from .user import User
 class Booking:
 
     def __init__(self, startdate, enddate, city, user):
-        return None
+        self.startdate=startdate
+        self.enddate=enddate
+        self.city = city
+        self.user = user
+        self.num_guests = 1
     
     def __repr__(self):
-        return None
+        str = " User: {0} City: {1} \n Start date: {2}\n End date: {3}\n Number of Guests: {4}"
+        str = str.format( self.user, self.city.name,self.startdate,
+            self.enddate, self.num_guests)
+        #str += " Num guests: %d"  % 
+        return str
