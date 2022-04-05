3a8ec8460e06e228af83ff09c24a605d5d156ab5
Cameron <cpanzenbock@users.noreply.github.com>
2022-04-05 12:53:31 +1000

    Task 1: created structure of user.py

---
+++ ctrlr/user.py
@@ -0,0 +1,10 @@
+class User:
+    # this is the function used to create the user
+    def __init__(self):
+        return None
+
+    def register(self,name,pwd,emailID):
+        return None
+
+    def __repr__(self):
+        return None
