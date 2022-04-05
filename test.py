69d3c6e601e8e9018e8bbe9413d82e1ba5dd5cfc
Cameron <cpanzenbock@users.noreply.github.com>
2022-04-05 13:41:08 +1000

    Task 4: corrected instantiate classes

--- test.py
+++ test.py
@@ -3,3 +3,16 @@ from ctrlr.user import User
 # from ctrlr.user import FrequentTraveller
 from ctrlr.booking import Booking
 from ctrlr.city import City
+
+user1 = User()
+user1.register('jill','iueo','jill@y.com')
+
+# su = FrequentTraveller()
+# su.register('jack','iituri', 'jack@x.com',123231)
+
+brisbane = City('Brisbane', 'City in Queensland with a good weather')
+
+st_date = datetime(2019,11,23)
+end_date = datetime(2019,11,30)
+
+booking = Booking(st_date,end_date,brisbane,user1)
