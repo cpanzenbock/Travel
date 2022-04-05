bc2b7eacfe1e723c152ab12922dbe1a1e944cb16
Cameron <cpanzenbock@users.noreply.github.com>
2022-04-05 13:14:06 +1000

    Task 4: instantiate test classes

--- test.py
+++ test.py
@@ -4,3 +4,15 @@ from travel.user import FrequentTraveller
 from travel.booking import Booking
 from travel.city import City
 
+user1 = User()
+user1.register('jill','iueo','jill@y.com')
+
+su = FrequentTraveller()
+su.register('jack','iituri', 'jack@x.com',123231)
+
+brisbane = City('Brisbane', 'City in Queensland with a good weather')
+
+st_date = datetime(2019,11,23)
+end_date = datetime(2019,11,30)
+
+booking = Booking(st_date,end_date,brisbane,user1)
