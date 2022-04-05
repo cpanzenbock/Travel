88625a135526d16ab8c35d20b99c29a62a9dc979
Cameron <cpanzenbock@users.noreply.github.com>
2022-04-05 12:55:27 +1000

    Task 2: create City structure

---
+++ ctrlr/city.py
@@ -0,0 +1,10 @@
+class City:
+
+    def __init__(self, name, description):
+        return None
+    
+    def get_city_details(self):
+        return None
+
+    def __repr__(self):
+        return None
